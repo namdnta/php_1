63c087650b9add60e06b769d06c0152c9b54c431
nmdntaa <144686774+nmdntaa@users.noreply.github.com>
2025-07-31 08:00:15 +0700

    belajar if else

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <h2>Hobi saya adalah <?=$hobi; ?> </h2>
     <h2>Saya berada di kelas <?=$class; ?></h2> -->
 
-    <!-- <?php
+ <!-- <?php
     $X = 10;
     $Y = 20;
 
@@ -31,16 +31,37 @@
     ?>
 
     <h2>Apakah Y dan X sama?</h2>
-    <h2><?=$hasil; ?></h2> -->
-   
+    <h2><?=$hasil; ?></h2>
+    -->
 
-    <?php
+    <!-- <?php
     for($i = 1; $i <=35; $i++){
         if ($i % 2 != 0) {
             echo $i . "<br>";
         }
     }
-    ?>
+    ?> -->
+
+<?php
+
+$nilai = 95;
+echo "<h3>Nilai kamu adalah: <span style='color:blue;'>$nilai</span></h3>";
+
+echo "<h4>Predikat nilai:</h4>";
+
+if ($nilai >= 90) {
+    echo "<span style='color:green; font-weight:bold;'>Nilai Anda A</span>";
+} elseif ($nilai >= 80) {
+    echo "<span style='color:blue; font-weight:bold;'>Nilai Anda B</span>";
+} elseif ($nilai >= 70) {
+    echo "<span style='color:orange; font-weight:bold;'>Nilai Anda C</span>";
+} elseif ($nilai >= 60) {
+    echo "<span style='color:brown; font-weight:bold;'>Nilai Anda D</span>";
+} else {
+    echo "<span style='color:red; font-weight:bold;'>Nilai Anda E</span>";
+}
+
+?>
 
 </body>
 </html>
